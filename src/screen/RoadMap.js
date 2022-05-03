4b7643d7798e03323bf3f59b4106683c42def95c
TylerMcCullough <tyler@lightbodies.io>
2022-05-03 13:29:55 -0700

    Bug fixes for Video

--- src/screen/RoadMap.js
+++ src/screen/RoadMap.js
@@ -11,7 +11,7 @@ const RoadMap = () => {
         ref={ref}
         >
             <h1>RoadMap</h1>
-            <p><video src={require("..src/Videos/first promotion.mp4").default} alt="Road Map" /></p>
+            <p><video src={require("../assets/Videos/first-promotion.mp4").default} alt="Road Map" /></p>
         </div>
     );
 };
