6e5541c6af1ee1df147c530f623a2e3dfd886568
TylerMcCullough <tyler@lightbodies.io>
2022-05-06 08:47:39 -0700

    promotion finale

--- src/screen/RoadMap.js
+++ src/screen/RoadMap.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React from "react";
 import { useInView } from "react-intersection-observer";
+import promotionUser from "../assets/Videos/first-promotion.mp4";
 
 const RoadMap = () => {
-    const { ref, inView, entry } = useInView({
-        threshold: 1,
-      });
-    return (
-        <div
-        className={"roadmap light-article-clone inview"}
-        ref={ref}
-        >
-            <h1>RoadMap</h1>
-            <p><video src={require("../assets/Videos/first-promotion.mp4").default} alt="Road Map" /></p>
-        </div>
-    );
+  const { ref, inView, entry } = useInView({
+    threshold: 1,
+  });
+
+  return (
+    <div className={"roadmap light-article-clone inview"} ref={ref}>
+      <h1>RoadMap</h1>
+      <p>
+        <video
+          className="promotion_Video"
+          src={promotionUser}
+          alt="Road Map"
+          controls
+          autoPlay
+        />
+      </p>
+    </div>
+  );
 };
 
 export default RoadMap;
